9e35214e9a8d584e9e31fe7cc5d9a03d0cce087c
Gregg Carey <gregg.carey@outlook.com>
2024-01-31 17:43:49 -0500

    study

---
+++ MyCode/chapter02/AsyncConsoleExample/NetworkHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncConsoleExample;
+
+internal class NetworkHelper
+{
+    internal async Task CheckNetworkStatusAsync()
+    {
+        Task t = NetworkCheckInteralAsync();
+
+        for (int i = 0; i < 8; i++)
+        {
+            Console.WriteLine("Top level method working...");
+            await Task.Delay(500);
+        }
+        await t;
+    }
+
+    private static async Task NetworkCheckInteralAsync()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+
+            Console.WriteLine($"Is Network Available? Answer: {isNetworkUp}");
+
+            await Task.Delay(100);
+        }
+    }
+
+
+}
+
+
+
--- MyCode/chapter02/AsyncConsoleExample/Program.cs
+++ MyCode/chapter02/AsyncConsoleExample/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AsyncConsoleExample;
+
+Console.WriteLine("Hello Async");
+NetworkHelper networkHelper = new();
+await networkHelper.CheckNetworkStatusAsync();
+Console.WriteLine("Main Method complete");
+Console.ReadKey();
